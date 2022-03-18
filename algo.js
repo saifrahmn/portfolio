575a5d873d46269ddc05e6732dc2fbcbd211afb6
saifrahmn <saifrahmn78@gmail.com>
2022-03-18 21:29:55 +0530

    update

---
+++ algo.js
@@ -0,0 +1,8 @@
+//sort algorithm
+//merge sort
+var a =[12,8,2,1,5,4];
+var len=a.length;
+var mid=len/2;
+for(var i=0;i<len;i++){
+    
+}
---
+++ app.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import Menu from './Menu';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hi, I am Saif Rahman
+        </p>
+        <div class="mid">
+        Engineering - Student - Enthusatic
+        </div>
+        <div class="menu">
+        <span class="blog">
+            Blog
+       </span>
+       </div>
+       <svg viewBox="0 0 120 120">
+                    <circle cx="60" cy="60" r="50"/>
+                </svg>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
